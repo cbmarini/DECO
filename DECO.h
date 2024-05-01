5d2305130e9c1d8b399f3fed22da762c78ddda9c
cbmarini <cbmarinissen@gmail.com>
2024-05-01 15:49:11 +0200

    Added the characters for the 6 dimensional SU(3) representations 6 and 6B.

--- DECO.h
+++ DECO.h
@@ -15,6 +15,7 @@ CFunction Scal,Scalar,Grav,Gravity,FIELD,fill,f,FieldStrength,FS,R,DF;
 Autodeclare CFunction char,spin,LH,RH,Dirac;
 Autodeclare Symbol x,y,z,rep,w,im,var,r,p;
 Symbol i,ii,jj,field,rep,mass,n,B,maxn;
+Symbol [3B],[6B];
 *--#]
 *--#[ HilbertSeries: 
 #procedure HilbertSeries(p)
@@ -514,6 +515,21 @@ id charSU3(1,n?) = 1;
 #do i=1,1
         id, once charSU3(B,n?) = 1/z1^n + z1^n/z2^n + z2^n;
         if ( match(charSU3(B,n?)) ) redefine i "0";
+        .sort
+#enddo
+
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*       Character of 6 dimensional representation (6 & [6B])
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#do i=1,1
+        id, once charSU3(6,n?) = z1^(-n)+z1^(2*n)+z2^(-2*n)+z1^n/z2^n+z2^n+z2^(2*n)/z1^(2*n);
+        if ( match(charSU3(6,n?)) ) redefine i "0";
+        .sort
+#enddo
+
+#do i=1,1
+        id, once charSU3([6B],n?) = z1^(-2*n)+z1^n+z1^(2*n)/z2^(2*n)+z2^(-n)+z2^n/z1^n+z2^(2*n);
+        if ( match(charSU3([6B],n?)) ) redefine i "0";
         .sort
 #enddo
 
