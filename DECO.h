bb407c1cbdd993f23587d7fb8dbfa3fff83afd46
cbmarini <cbmarinissen@gmail.com>
2024-08-07 15:31:41 +0200

    Added the possibility to add any SU(2) represenation, so not only limited to representations up to 4 anymore.

--- DECO.h
+++ DECO.h
@@ -474,8 +474,8 @@ id charSU2(1,n?) = 1;
 #enddo
 
 #do i=1,1
-        id, once charSU2(4,n?) = y^(3*n)+y^(1*n)+y^(-1*n)+y^(-3*n);
-        if ( match(charSU2(4,n?)) ) redefine i "0";
+        id, once charSU2(rep?{>3},n?) = sum_(jj,-(rep-1),rep-1,2,y^(jj*n));
+        if ( match(charSU2(rep?{>3},n?)) ) redefine i "0";
         .sort
 #enddo
 
